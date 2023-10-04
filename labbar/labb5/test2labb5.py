2a0e71804470aebfd5c22a51de4636c9773afd36
Teo-Backstrom <teo.backstrom@gmail.com>
2023-10-04 19:37:00 +0200

    labb5extra

--- labbar/labb5/test2labb5.py
+++ labbar/labb5/test2labb5.py
@@ -3,42 +3,172 @@
 
 class Person:
     """
-    Person klass med för och efternamn samt person nummer som egenskaper
+    Person klass med för och lastname samt person nummer som egenskaper
     """
 
     # sparar egenskaperna hos objektet
-    def __init__(self, förnamn, efternamn, person_nr):
-        self.förnamn = förnamn
-        self.efternamn = efternamn
+    def __init__(self, firstname, lastname, person_nr):
+        self.firstname = firstname
+        self.lastname = lastname
         self.person_nr = person_nr
-        self.roll = ""
+        self.roll = "None"
 
     # bestämmer hur objektet representeras som en sträng
     def __str__(self):
-        return f"Namn: {self.förnamn} {self.efternamn} Personnr: {self.person_nr}"
+        return f"Namn: {self.firstname} {self.lastname} Personnr: {self.person_nr} Roll: {self.roll}"
+
+    def rename(self):
+        control_quest = ""
+
+        while control_quest != ("j" or "n"):
+            control_quest = input(
+                f"Vill du ändra namn på {self.firstname} {self.lastname} (j/n)?"
+            ).lower()
+        if control_quest == "j":
+            firstname, lastname = nameinput(
+                text="Skriv in det nya namnet:", wordcount=2
+            )
+            self.firstname = firstname
+            self.lastname = lastname
+            print(
+                f"Nu är namnet för {self.person_nr} ändrat till {firstname} {lastname}!"
+            )
+
+        else:
+            print("Namnnet är inte ändrat")
 
 
 class Student(Person):
-    def __init__(self, förnamn, efternamn, person_nr):
-        super().__init__(förnamn, efternamn, person_nr)
+    def __init__(self, firstname, lastname, person_nr):
+        super().__init__(firstname, lastname, person_nr)
         self.roll = "Student"
 
 
 class Teacher(Person):
-    def __init__(self, förnamn, efternamn, person_nr):
-        super().__init__(förnamn, efternamn, person_nr)
+    def __init__(self, firstname, lastname, person_nr):
+        super().__init__(firstname, lastname, person_nr)
         self.roll = "Lärare"
 
 
 class School:
-    students = []
-    teachers = []
+    def __init__(self):
+        self.students = []
+        self.teachers = []
 
-    def __init__(self, person, roll):
-        if roll == "Lärare":
-            self.teachers.append(person)
+    def print_students(self):
+        for student in self.students:
+            print(student)
+
+    def print_teachers(self):
+        for teacher in self.teachers:
+            print(teacher)
+
+    def addperson(self):
+        print("Är personen en Lärare eller Student?")
+        role = ""
+        while role.lower() not in ["lärare", "student"]:
+            role = input()
+            if role.lower() not in ["lärare", "student"]:
+                print('Du måste ange "Lärare" eller "Student"')
+        firstname, lastname = nameinput(
+            text="Vad heter studenten? (firstname lastname)", wordcount=2
+        )
+        person_nr = person_nr_input()
+
+        if role.lower() == "lärare":
+            self.teachers.append(Teacher(firstname, lastname, person_nr))
         else:
-            self.students.append(person)
+            self.students.append(Student(firstname, lastname, person_nr))
+
+    def rename_person(self):
+        person_n = person_nr_input()
+
+        for student in self.students:
+            if person_n == student.person_nr:
+                person = student
+        for teacher in self.teachers:
+            if person_n == teacher.person_nr:
+                person = teacher
+        person.rename()
+
+    def remove_person(self):
+        person_n = person_nr_input()
+        person = None
+        for student in self.students:
+            if person_n == student.person_nr:
+                person = student
+        for teacher in self.teachers:
+            if person_n == teacher.person_nr:
+                person = teacher
+
+        if person == None:
+            print("Ingen person hittades")
+        else:
+            control_quest = ""
+
+            while control_quest != ("j" or "n"):
+                control_quest = input(
+                    f"Vill du ta bort {person.firstname} {self.lastname} (j/n)?"
+                ).lower()
+
+                if control_quest == "j":
+                    self.students.remove(person)
+                    self.teachers.remove(person)
+                    print(
+                        f"Nu är {person.person_nr}: {person.firstname} {person.lastname} borttagen!"
+                    )
+            else:
+                print("Inget har ändrats")
+
+    def search_people(self):
+        people = []
+        firstname, lastname = nameinput(text="Vem vill du söka efter", wordcount=2)
+        for teacher in self.teachers:
+            if teacher.firstname.lower() == firstname.lower() and (
+                teacher.lastname.lower() == lastname.lower()
+            ):
+                people.append(teacher)
+        for student in self.students:
+            if student.firstname.lower() == firstname.lower() and (
+                student.lastname.lower() == lastname.lower()
+            ):
+                people.append(student)
+        for person in people:
+            print(person)
+
+
+def nameinput(text="", wordcount=1):
+    name = input(text)
+    # Kollar att det är rätt antal mellanslag
+    if len(name.split(" ")) != wordcount:
+        # Ger konstruktiv feedback
+        print(f"inmatningen måste vara uppdalad i {wordcount} del/delar")
+    else:
+        return name.split(" ")
+
+
+def person_nr_input():
+    allowedinput = False
+    monthday = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    while not allowedinput:
+        person_nr = input("Mata in personnummer (YYMMDDXXXX)")
+
+        try:
+            int(person_nr)
+        except:
+            print("Personnummret får bara innehålla siffor")
+        else:
+            month = int(person_nr[2:4])
+            day = int(person_nr[4:6])
+            if len(person_nr) != 10:
+                print("Skriv personnummret med 10 siffror")
+            elif month < 1 or month > 12:
+                print(f"Månaden {person_nr[2:4]} finns inte")
+            elif day < 1 or day > monthday[month - 1]:
+                print("Dagen finns inte i vald månad")
+            else:
+                allowedinput = True
+    return person_nr
 
 
 def inläsning(typ=any, text="", ordtal=1):
@@ -62,15 +192,43 @@ def inläsning(typ=any, text="", ordtal=1):
             # Kollar att det är rätt antal mellanslag
             if len(värde.split(" ")) != ordtal:
                 # Ger konstruktiv feedback
-                print("Skriv förnamn och efternamn skillt av ett mellanslag")
+                print(f"inmatningen måste vara uppdalad i {ordtal} del/delar")
             else:
                 return värde
 
 
 def main():
-    """
-    program för att ta in personerser och dess egenskapar och sedan skriva ut dem
-    """
+    school = School()
+
+    value = ""
+    while value != "ö":
+        value = input(
+            "1. Vill du lägga till (l)\n2 Ändra (a)\n3 Ta bort (t)\n3 Söka ett objekt(s)\n4 Skriva ut alla studenter(ss)\n5 Skriva ut alla lärare(sl)\n6 Avsluta (ö) för att avsluta"
+        )
+        if value == "l":
+            school.addperson()
+        elif value == "a":
+            school.rename_person()
+        elif value == "t":
+            school.remove_person()
+        elif value == "s":
+            school.search_people()
+        elif value == "ss":
+            school.print_students()
+        elif value == "sl":
+            school.print_teachers()
+        else:
+            print("felaktig input")
+
+    print("Hejdå")
+
+
+main()
+
+""" def main():
+    
+    #program för att ta in personerser och dess egenskapar och sedan skriva ut dem
+    
     # lista för att spara objekten i
 
     # Tar in 3st personer
@@ -81,26 +239,26 @@ def main():
         if val1 == "Lärare":
             # tar in namn
             namn = inläsning(
-                typ=str, text="Vad heter studenten? (förnamn efternamn)", ordtal=2
+                typ=str, text="Vad heter studenten? (firstname lastname)", ordtal=2
             )
-            # Delar på namnen till för och efternamn
-            förnamn, efternamn = namn.split(" ")
+            # Delar på namnen till för och lastname
+            firstname, lastname = namn.split(" ")
             # tar in personnr
             person_nr = inläsning(typ=int, text="Vad är studentens personnr?")
             # sparar objektet i listan
-            School(Teacher(förnamn, efternamn, person_nr), val1)
+            School(Teacher(firstname, lastname, person_nr), val1)
             print("\nObjektet skapat!\n")
         else:
             # tar in namn
             namn = inläsning(
-                typ=str, text="Vad heter studenten? (förnamn efternamn)", ordtal=2
+                typ=str, text="Vad heter studenten? (firstname lastname)", ordtal=2
             )
-            # Delar på namnen till för och efternamn
-            förnamn, efternamn = namn.split(" ")
+            # Delar på namnen till för och lastname
+            firstname, lastname = namn.split(" ")
             # tar in personnr
             person_nr = inläsning(typ=int, text="Vad är studentens personnr?")
             # sparar objektet i listan
-            School(Student(förnamn, efternamn, person_nr), val1)
+            School(Student(firstname, lastname, person_nr), val1)
             print("\nObjektet skapat!\n")
 
     print("Här är alla studenter på KTH:")
@@ -114,4 +272,4 @@ def main():
 
 
 # Kör mainfunktionen
-main()
+main() """
