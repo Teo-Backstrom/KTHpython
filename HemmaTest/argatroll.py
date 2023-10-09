90d51d0df0988de3dc0578667496fa091cea4f61
Teo-Backstrom <teo.backstrom@gmail.com>
2023-10-09 12:35:25 +0200

    felkontroll

--- HemmaTest/argatroll.py
+++ HemmaTest/argatroll.py
@@ -1,13 +1,49 @@
-y = []
+
+def checksurround(y, x, A):
+    if A[y][x] == "|*":
+        print("Är samma")
+        return True
+    for i in range(y-1, y+1):
+        for j in range(x-1, x+1):
+            if A[i][j] == "|*":
+                print("Går inte")
+                return True
+    
+    return False
+
+
+A = []
 n  = 5
 for i in range(n):
     x = []
     for j in range(n):
         x.append("|_")
-    y.append(x)
+    A.append(x)
         
 for i in range(n):
     
     for j in range(n):
-        print(y[i][j], end="")
+        print(A[i][j], end="")
     print("|")
+    
+val = input("Val")
+while val != "ö":
+    
+    y, x = val.split(" ")
+    y = int(y) - 1
+    x = int(x) - 1
+    while(checksurround(y,x,A)):
+        val = input("Val")
+        y, x = val.split(" ")
+        y = int(y) - 1
+        x = int(x) - 1
+        
+    A[y][x] = "|*"
+    for i in range(n):
+        for j in range(n):
+            print(A[i][j], end="")
+        print("|")
+    val = input("Val")
+
+
+    
